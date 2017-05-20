1bb523d15cd114e2fa1afc7969e13538e921b1b8
kora j singh <kora@singh.com>
2017-05-20 08:59:11 +0100

    This is my first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First Git File</title>
+<link rel="stylesheet" type="text/css" href="asset/style.css">
+</head>
+<body>
+<div class="try">
+	<ul>
+		<li>First</li>
+		<li>Second</li>
+		<li>third</li>
+		<li>Fourth</li>
+	</ul>
+</div>
+
+</body>
+</html>
